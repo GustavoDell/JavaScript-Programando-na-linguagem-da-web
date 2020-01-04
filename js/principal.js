0d41103488f0a1a1e2c7b2a977bf8c0a4710d709
GustavoDell <gustavo.dell@outlook.com>
2020-01-04 20:00:49 -0300

    Adicionando paciente na tabela via form

--- js/principal.js
+++ js/principal.js
@@ -61,7 +61,38 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     
     event.preventDefault();// Função que previne o comportamento padrão do browser(Está função remove o comportamento padrão já inserido no elemento)
-    console.log("Oi eu sou um botão e fui clicado")
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");//criado tags no html  via javascript
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent= gordura;
+
+    pacienteTr.appendChild(nomeTd);//inserindo as tags filhas na tag pai
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+    console.log(pacienteTr);
+
 });
 
 
@@ -70,3 +101,4 @@ botaoAdicionar.addEventListener("click", function(event){
 
 
 
+ 
