040324d1b7d5490c44ef6f05a72100042d67450f
GustavoDell <gustavo.dell@outlook.com>
2020-02-28 16:25:04 -0300

    08.Filtrando uma tabela

--- js/filtra.js
+++ js/filtra.js
@@ -1,12 +1,29 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-    console.log(this.value);
-    var paciente = document.querySelectorAll(".paciente");
+    console.log(this.value);// this é referente ao dono do evento
+    var pacientes = document.querySelectorAll(".paciente");
 
-    for(var i= 0; i < paciente; i++){
-        var paciente = paciente[i]
-        var tdNome = paciente.querySelector(".info-nome");
-        nome = paciente.textContent;
+    if(this.value.length > 0){
+        console.log("Tem algo digitado")
+        for(var i= 0; i < pacientes.length; i++){
+            var paciente = pacientes[i]
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value,"i")//criando uma expressão regular
+            if(!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else{
+        for(i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i]
+            paciente.classList.remove("invisivel");
+        }
     }
+
+    
+    
 });
